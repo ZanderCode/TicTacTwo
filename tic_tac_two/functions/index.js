613189159c3f5c3f7c1b5621614f0f799282d3c5
Zander <alexlamarche90@gmail.com>
2025-06-30 19:46:01 -0400

    auth credentials handled via cloud function, no more secrets on client

--- tic_tac_two/functions/index.js
+++ tic_tac_two/functions/index.js
@@ -7,11 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {setGlobalOptions} = require("firebase-functions");
+const {setGlobalOptions, params, config} = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// admin.initializeApp();
+const admin = require("firebase-admin");
+const axios = require("axios");
+
+admin.initializeApp();
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -28,7 +31,38 @@ setGlobalOptions({ maxInstances: 10 });
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
+
+// Check if running in emulator
+// ran firebase functions:config:set google.client_id="YOUR_CLIENT_ID" google.client_secret="YOUR_CLIENT_SECRET"
+
+exports.getTokenData = onRequest(async (request, response) => {
   logger.info("Hello logs!", {structuredData: true});
-  response.send("emulated hello world function");
+
+  const { code, redirectUri } = request.body;
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret =  process.env.GOOGLE_CLIENT_SECRET;
+
+  const tokenRes = await axios.post("https://oauth2.googleapis.com/token", null, {
+    params: {
+      code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      grant_type: "authorization_code",
+    },
+  });
+
+  const { id_token, access_token} = tokenRes.data;
+
+  const payload = JSON.parse(
+    Buffer.from(id_token.split(".")[1], "base64").toString("utf8")
+  );
+
+  const uid = payload.sub;
+
+  // Step 3: Create Firebase custom token
+  const customToken = await admin.auth().createCustomToken(uid);
+
+  // Step 4: Return the Firebase custom token
+  response.json({ firebaseToken: customToken, googleIdToken: id_token, googleAccessToken: access_token, });
 });
